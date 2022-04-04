f2ec42405c29bf8601dee9a6e79350b77ec84556
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-04-04 16:58:57 +0530

    Added solution - LeetHub

---
+++ 26. Check if a String is Subsequence of Other - GFG/26.-check-if-a-string-is-subsequence-of-other.cpp
@@ -0,0 +1,48 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to check if a string is subsequence of other string.
+    bool isSubSequence(string A, string B)
+    {
+        //code here
+        // Iterative Approach
+        
+        int j=0;
+        int m = A.length();
+        int n = B.length();
+        
+        for(int i=0;i<n && j<m;i++){
+            if(A[j]==B[i])
+            j++;
+        }
+        return (j==m);
+    }
+};
+
+// { Driver Code Starts.
+    
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    string A, B; cin>>A>>B;
+	    Solution ob;
+	    bool ans = ob.isSubSequence(A, B);
+	    if(ans == true)
+	        cout<<"True"<<endl;
+	    else
+	        cout<<"False"<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
