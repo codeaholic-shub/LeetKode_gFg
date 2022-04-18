c37e683b3f63cff0df89b9b75944b49d831082b8
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-04-18 11:17:53 +0530

    Added solution - LeetHub

--- 1. Naive Pattern Search - GFG/1.-naive-pattern-search.cpp
+++ 1. Naive Pattern Search - GFG/1.-naive-pattern-search.cpp
@@ -16,19 +16,18 @@ class Solution
     { 
     	
     	// Your code here
-    		int m=pat.length();
-	int n=txt.length();
-	for(int i=0;i<=n-m;i++){
-	    int j=0;
-	    for(j=0;j<m;j++)
-	        if(txt[i+j]!=pat[j])
-	        break;
-	   if(j==m)
-	   return true;
-	    
-	}
-	return false;
+    	int m = pat.length();
+    	int n = txt.length();
     	
+    	for(int i=0;i<=n-m;i++){
+    	    int j;
+    	    for(j=0;j<m;j++)
+    	    if(txt[i+j]!=pat[j])
+    	    break;
+    	    
+    	if(j==pat.length())return 1;
+    	}
+    	return 0;
     }
 };
 
