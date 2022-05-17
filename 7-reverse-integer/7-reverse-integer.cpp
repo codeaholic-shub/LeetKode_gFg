42fffcc6a10ef3746fad86159f2512160eb1ff28
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-05-17 20:54:15 +0530

    Time: 4 ms (42.20%), Space: 5.9 MB (34.17%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int temp=0;
+        while(x){
+            if(temp>INT_MAX/10 || temp<INT_MIN/10)return 0;
+            temp=x%10+temp*10;
+            x/=10;
+        }
+        return temp;
+    }
+};
