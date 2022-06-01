93ae0143f16b6ca05ae974974999edca6204e36b
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-06-01 11:31:30 +0530

    Time: 14 ms (7.39%), Space: 12.1 MB (37.71%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-void nextPermutation(vector<int>& nums) {
-//if there is single element
-if(nums.size()==1)
-return;
-
-    //index1 store the least value index and we will travel by last element
-    int index1;
-    for(int i=nums.size()-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
-            index1=i;
-            break;
-        }
-    }
-    
-    //if index1 is not fill then just sort or reverse it because the numberis already big we can't find next big no
-    if(index1<0)
-        reverse(nums.begin(),nums.end());
-    
-    //else find next big no then index1 and store in index2
-    else{
-        int index2=0;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]>nums[index1]){
-                index2=i;
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size(),k,l;
+        for(k=n-2;k>=0;k--){
+            if(nums[k]<nums[k+1]){
                 break;
             }
         }
-        // just swap index1 and index
-        swap(nums[index1],nums[index2]);
-        //sort element from index1 to last no
-        sort(nums.begin()+index1+1,nums.end());
+        if(k<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(l=n-1;l>k;l--){
+                if(nums[l]>nums[k]){
+                    break;
+                }
+            }
+        swap(nums[l],nums[k]);
+        reverse(nums.begin()+k+1,nums.end());
+        }
+       
     }
-}
 };
