7ef7953eafc26c4da4a47f6cfe64b4c4d9fbb264
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-07-11 21:30:07 +0530

    Time: 101 ms (18.05%), Space: 24.1 MB (33.87%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool checkPalin(string s,int low,int high){
         while(low<high){
-        if(s[low]!=s[high]){
-            return false;
+            if(s[low]!=s[high])
+                return false;
+            low++;
+            high--;
         }
-        low++;
-        high--;
-        }
-    return true;
+        return true;
     }
     bool validPalindrome(string s) {
-        int low=0,high=s.length()-1;
+        int n = s.length()-1;
+        int low = 0,high = n;
         while(low<high){
-            if(s[low]!=s[high]){
-                return checkPalin(s,low,high-1) || checkPalin(s,low+1,high);
-            }
+            if(s[low]!=s[high])
+                return checkPalin(s,low+1,high) || checkPalin(s,low,high-1);
             low++;
             high--;
         }
