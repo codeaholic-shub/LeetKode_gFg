d7a36b85525385e3a203e821fd06133b241956c1
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-07-12 18:10:58 +0530

    Time: 3 ms (35.48%), Space: 6 MB (42.16%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string maximumTime(string time) {
+        string res = "";
+        if(time[0]=='?'){
+            time[0]=time[1]<='3' || time[1]=='?'?'2':'1';
+        }
+        if(time[1]=='?'){
+            time[1]=time[0]=='2'?'3':'9';
+        }
+        if(time[3]=='?'){
+            time[3]='5';
+        }
+        if(time[4]=='?'){
+            time[4]='9';
+        }
+        return time;
+    }
+};
