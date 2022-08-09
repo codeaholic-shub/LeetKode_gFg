1f13547b4df12f4a78ce8351e10e8c231385d59e
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-08-09 22:13:41 +0530

    Time: 6 ms (24.15%), Space: 8.2 MB (70.34%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // Dutch National Flag Alorithm
+        
+        
         int cnt0=0,cnt1=0,cnt2=0;
+        
+        
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0)cnt0++;
             if(nums[i]==1)cnt1++;
             if(nums[i]==2)cnt2++;
         }
-       
-       int j=0;
+        int i=0;
         while(cnt0!=0){
-            nums[j]=0;
+            nums[i]=0;
+            i++;
             cnt0--;
-            j++;
         }
         while(cnt1!=0){
-            nums[j]=1;
+            nums[i]=1;
+            i++;
             cnt1--;
-            j++;
         }
         while(cnt2!=0){
-           nums[j]=2;
+            nums[i]=2;
+            i++;
             cnt2--;
-            j++;
         }
-      
+    
     }
 };
