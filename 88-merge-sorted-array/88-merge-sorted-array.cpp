f2d52b6d6de63b47bdce0d237007adc72dbbab35
Shubhankar Poddar <65807708+codeaholic-shub@users.noreply.github.com>
2022-08-17 23:35:14 +0530

    Time: 4 ms (54.85%), Space: 9.3 MB (30.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,13 +4,13 @@ public:
         int i=0,j=0;
         vector<int>temp;
         while(i<m && j<n){
-            if(nums1[i]>nums2[j]){
-                temp.push_back(nums2[j]);
-                j++;
-            }
-            else{
+            if(nums1[i]<nums2[j]){
                 temp.push_back(nums1[i]);
                 i++;
+            }
+            else{
+                temp.push_back(nums2[j]);
+                j++;
             }
         }
         while(i<m){
